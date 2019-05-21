100f1f4805774d216bf03e6a00977f3a963eac52
sauravkumar2014 <sauravdahiya2010@gmail.com>
2019-05-21 21:56:17 +0530

    Classic Concurrency Problem

--- basic_thread.cpp
+++ basic_thread.cpp
@@ -1,13 +1,29 @@
+/*
+ * Added the screenshot of commit-2
+ * Every thread appends to cout without caring about any other thread
+ * Hence messed up output due to no control over order of execution and preemption
+*/
+
 #include <thread>
 #include <iostream>
+#include <vector>
 
 void hello(){
-    std::cout << "Hello from thread " << std::endl;
+    std::cout << "Hello from thread " << std::this_thread::get_id() << std::endl;
 }
 
 int main(){
-    std::thread t1(hello);
-    t1.join();
+    std::vector<std::thread> threads;
+
+    for(int i = 0; i < 5; ++i){
+        threads.push_back(std::thread(hello));
+    }
+
+    for(auto& thread : threads){
+    	//thread.detach for running as background thread independently
+    	//usually check if thread is joinable
+        thread.join();
+    }
 
     return 0;
 }
